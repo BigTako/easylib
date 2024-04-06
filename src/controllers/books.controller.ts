c6348f22d8fc51aaa77e82607d7fe2af1b335dab
BigTako <libert761@gmail.com>
2024-04-06 16:02:56 +0300

    added book CRUD

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -3,17 +3,39 @@ import { catchAsync } from '../utils/catchAsync'
 import { prisma } from '../utils/db'
 
 export const getBooks = catchAsync(async (req: Request, res: Response) => {
-  const docs = prisma.book.findMany()
+  const docs = await prisma.book.findMany()
   res.status(200).json(docs)
 })
 
 export const createBook = catchAsync(async (req: Request, res: Response) => {
+  console.log({ body: req.body })
   const { title, author } = req.body
-  const doc = prisma.book.create({
+  const doc = await prisma.book.create({
     data: {
       title,
       author
     }
   })
   res.status(201).json(doc)
+})
+
+export const updateBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { title, author } = req.body
+  const doc = await prisma.book.update({
+    where: { id },
+    data: {
+      title,
+      author
+    }
+  })
+  res.status(200).json(doc)
+})
+
+export const deleteBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  await prisma.book.delete({
+    where: { id }
+  })
+  res.status(204).json(null)
 })
--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,10 +1,10 @@
-import { createBook, getBooks } from '../controllers/books.controller'
+import { createBook, deleteBook, getBooks, updateBook } from '../controllers/books.controller'
 
 import express from 'express'
 // const express = require('express')
 
 const router = express.Router()
 
-router.get('/', getBooks).post('/', createBook)
+router.get('/', getBooks).post('/', createBook).put('/:id', updateBook).delete('/:id', deleteBook)
 
 export { router as booksRoutes }
